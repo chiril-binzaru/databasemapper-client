7d5a6e05c29b094d209ac581fc412820afb1bcc2
Chiril Binzaru <kirill.colegiu.p2213@gmail.com>
2026-05-25 10:09:53 +0300

    Make JsonAssertExampleTest class public so that IntelliJ sees it and its methods as tests

--- src/test/java/io/github/chirilbinzaru/databasemapper/client/example/JsonAssertExampleTest.java
+++ src/test/java/io/github/chirilbinzaru/databasemapper/client/example/JsonAssertExampleTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-class JsonAssertExampleTest {
+public class JsonAssertExampleTest {
     @Test
     void printsReadableJsonMismatches() {
         String expectedFromDatabase = """
