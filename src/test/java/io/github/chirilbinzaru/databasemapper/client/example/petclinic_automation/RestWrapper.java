eec37d2fbf48aa59d49b19d30b171d87a238d603
Chiril Binzaru <kirill.colegiu.p2213@gmail.com>
2026-06-07 17:09:48 +0300

    Add the ability to fetch model lists from RestWrapper

--- src/test/java/io/github/chirilbinzaru/databasemapper/client/example/petclinic_automation/RestWrapper.java
+++ src/test/java/io/github/chirilbinzaru/databasemapper/client/example/petclinic_automation/RestWrapper.java
@@ -1,8 +1,13 @@
 package io.github.chirilbinzaru.databasemapper.client.example.petclinic_automation;
 
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class RestWrapper {
 
@@ -19,6 +24,13 @@ public class RestWrapper {
                 .extract().as(type);
     }
 
+    public <T> List<T> getList(String path, Class<T> elementClass) {
+        return request()
+                .when().get(url(path))
+                .then().statusCode(200)
+                .extract().as(listTypeRef(elementClass));
+    }
+
     public <T> T post(String path, Object body, Class<T> type) {
         return request()
                 .body(body)
@@ -27,12 +39,33 @@ public class RestWrapper {
                 .extract().as(type);
     }
 
+    public <T> List<T> postList(String path, Object body, Class<T> elementClass) {
+        return request()
+                .body(body)
+                .when().post(url(path))
+                .then().statusCode(200)
+                .extract().as(listTypeRef(elementClass));
+    }
+
     public <T> T put(String path, Object body, Class<T> type) {
         return request()
                 .body(body)
                 .when().put(url(path))
                 .then().statusCode(200)
                 .extract().as(type);
+    }
+
+    private <T> TypeRef<List<T>> listTypeRef(Class<T> elementClass) {
+        return new TypeRef<List<T>>() {
+            @Override
+            public Type getType() {
+                return new ParameterizedType() {
+                    public Type[] getActualTypeArguments() { return new Type[]{elementClass}; }
+                    public Type getRawType() { return List.class; }
+                    public Type getOwnerType() { return null; }
+                };
+            }
+        };
     }
 
     private RequestSpecification request() {
